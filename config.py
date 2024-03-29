f948ec8ae97c8ccf238aeaa78f3f8f232afd5067
Максим Валерьевич <maxdompc@gmail.com>
2024-03-29 18:35:33 +0500

    Создан бот для перевода валют

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '6659377078:AAGWPcjJsOKjCIsLvvGvelxeNr1xJpw0oDo'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+
+}
